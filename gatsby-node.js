3776e63241a0de381a39c76324a0dceaf5ac1c44
NickHellemans <60689153+NickHellemans@users.noreply.github.com>
2020-10-27 12:48:50 +0100

    create header component en index page

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,40 @@
  */
 
 // You can delete this file if you're not using it
+const { createRemoteFileNode } = require(`gatsby-source-filesystem`)
+
+/* Aan de hand van dit stukje code worden de images vanuit WPgraphql omgezet tot images waarop Gatsby image optimization kan toepassen */
+exports.createResolvers = async ({
+  actions,
+  cache,
+  createNodeId,
+  createResolvers,
+  store,
+  reporter,
+}) => {
+  const { createNode } = actions
+
+  await createResolvers({
+    WPGraphql_MediaItem: {
+      imageFile: {
+        type: "File",
+        async resolve(source) {
+          let sourceUrl = source.sourceUrl
+
+          if (source.mediaItemUrl !== undefined) {
+            sourceUrl = source.mediaItemUrl
+          }
+
+          return await createRemoteFileNode({
+            url: encodeURI(sourceUrl),
+            store,
+            cache,
+            createNode,
+            createNodeId,
+            reporter,
+          })
+        },
+      },
+    },
+  })
+}
